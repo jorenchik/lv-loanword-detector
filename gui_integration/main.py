dee413bdc86ddcc1e0bed022d9ca64843e37655f
Easyjo4 <rainers.kozlovskis@outlook.lv>
2025-06-14 18:12:45 +0300

    Somewhat complete layout for tab1

--- gui_integration/main.py
+++ gui_integration/main.py
@@ -4,7 +4,9 @@ import sys
 
 # sudo apt-get install python3-tk  Wtf linux? Idk how to add this dependency to the project config
 import tkinter as tkint
+import tkinter.ttk as tkintTtk
 import tkinter.font as tkintFont
+tk = tkint
 
 import gui_integration.utils as utils
 
@@ -29,13 +31,29 @@ def main():
     tk_root.title("mordoria")
     tk_root.geometry( f"{window_extent[0]}x{window_extent[1]}" )
 
-    # frame = tkint.Frame(tk_root, width=300, height=300)
-    root_frame = tkint.Frame(tk_root)
-    root_frame.grid(padx=10, pady=10)
-    tkint.Label(root_frame, text="mordoria").grid(row=0, column=0)
-    tkint.Button(root_frame, text="Some button").grid(row=1, column=0)
+    tk_default_font = tkintFont.nametofont("TkDefaultFont")
+    font_title = tk_default_font.copy()
+    font_title.configure(size=12)
 
-    frame_text = tkint.Frame(root_frame)
+    root_frame = tkint.Frame(tk_root)
+    root_frame.pack(padx=5, pady=5)
+
+    ctx_tab_navbar = tkintTtk.Notebook(root_frame)
+    ctx_tab_navbar.pack(side="top", fill="x")
+
+
+    # TAB 1
+    ctx_tab1 = tk.Frame(ctx_tab_navbar)
+    ctx_tab_navbar.add(ctx_tab1, text="  TAB 1  ")
+    root_frame = ctx_tab1
+
+    tkint.Label(root_frame, text="Some title", justify="left", font=font_title).pack(padx=5, pady=5, anchor="w")
+    tkint.Button(root_frame, text="Some button").pack(side="bottom")
+
+    frame_contents = tkint.Frame(root_frame)
+    frame_contents.pack()
+
+    frame_text = tkint.Frame(frame_contents)
     frame_text.grid(column=0, row=0)
     ctx_text_area = tkint.Text(
         frame_text, width=64, height=16, wrap="word", autoseparators=True, undo=True, maxundo=-1
@@ -48,30 +66,37 @@ def main():
     ctx_text_slider.pack(side="right", fill="y")
     ctx_text_area.configure(yscrollcommand=ctx_text_slider.set)
 
-
-    frame_controlls = tkint.Frame(root_frame, background="cyan")
+    frame_controlls = tkint.Frame(frame_contents, background="cyan")
     frame_controlls.grid(column=1, row=0, sticky="nsew")
-    tkint.Label(frame_controlls, text="mordoria").grid(row=0, column=0)
-
 
     frame_parambox_1 = tkint.Frame(
         frame_controlls, padx=5, pady=5, background='', highlightbackground="black", highlightthickness=1)
-    frame_parambox_1.grid(column=0, row=0, padx=5, pady=5)
-    tkint.Label(frame_parambox_1, text="Model \ntreshold ").grid(row=0, column=0)
+    frame_parambox_1.grid(column=0, row=0, padx=5, pady=5, sticky='nsew')
+    tkint.Label(frame_parambox_1, text="Model \ntreshold").grid(row=0, column=0)
     ctx_model_scale = tkint.Scale(frame_parambox_1, from_=0.0, to=1.0, digits=3, resolution=0.01, orient="horizontal", length=150)
     ctx_model_scale.grid(row=0, column=1)
     ctx_model_scale.set(0.69)
 
-    # pw_label = Label(root, text="Password").grid(row=0, column=0, pady=4, padx=4)
-    # length_label = Label(root, text="Length").grid(row=2, column=0, pady=4, padx=4)
-    # pw_input = Entry(root, width=50)
-    # length_input = Scale(root, from_=8, to=50, orient=HORIZONTAL, length=300)
-    # length_input.set(30)
-    # pw_input.grid(row=0, column=1, pady=4, padx=4)
-    # length_input.grid(row=2, column=1, pady=4, padx=4)
-    # Button(root, text='Quit', command=root.quit).grid(row=3, column=2, sticky=W, pady=4)
-    # Button(root, text='Generate', command=rand_pw).grid(row=2, column=2, sticky=W, pady=4)
-    # Button(root, text='Copy', command=copy).grid(row=0, column=2, sticky=W, pady=4)
+    frame_parambox_2 = tkint.Frame(
+        frame_controlls, padx=5, pady=5, background='', highlightbackground="black", highlightthickness=1)
+    frame_parambox_2.grid(column=0, row=1, padx=5, pady=5, sticky='nsew')
+
+    tkint.Label(frame_parambox_2, text="Model type").grid(row=0, column=0)
+    ctx_model_type = tkintTtk.Combobox(
+        frame_parambox_2, state="readonly", values=["LR - Logical Regression", "RF - Random Forest", "math.Random()", "Chat-GPT", "Some Indian"] )
+    ctx_model_type.current(0)
+    ctx_model_type.grid(row=0, column=1)
+
+
+    # TAB 2
+    ctx_tab2 = tk.Frame(ctx_tab_navbar)
+    ctx_tab_navbar.add(ctx_tab2, text="  TAB 2  ")
+    root_frame = ctx_tab2
+
+    tkint.Label(
+        root_frame, text="Literary a second tab, this shit is insane", justify="left", font=font_title).pack(padx=5, pady=5, anchor="w")
+
+
 
     tk_root.mainloop()
 
